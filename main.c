92f2a1ff96113d79def3e5b306a7b8787fe98573
Alex <alebarrod@alum.us.es>
2017-05-01 11:11:23 +0200

    Completo el metodo world_get_cell y elimino comentarios basura.

--- main.c
+++ main.c
@@ -8,8 +8,8 @@
 void world_init(/* Recibo un mundo */);
 void world_print(bool w[W_SIZE_X][W_SIZE_Y]);
 void world_step(/* Recibo dos mundos */);
-int world_count_neighbors(/* Recibo un mundo y unas coordenadas */);
-bool world_get_cell(/* Recibo un mundo y unas coordenadas */);
+int world_count_neighbors(bool world_a[W_SIZE_X][W_SIZE_Y],int i,int j);
+bool world_get_cell(bool world_a[W_SIZE_X][W_SIZE_Y],int i,int j);
 void world_copy(bool world_a[W_SIZE_X][W_SIZE_Y],bool world_b[W_SIZE_X][W_SIZE_Y]);
 
 int main()
@@ -72,19 +72,44 @@ void world_step(/* Recibo dos mundos */)
 	 *
 	 * - Copiar el mundo auxiliar sobre el mundo principal
 	 */
+	
 }
 
-int world_count_neighbors(/* Recibo un mundo y unas coordenadas */)
+int world_count_neighbors(bool world_a[W_SIZE_X][W_SIZE_Y],int i,int j)
 {
-	// Devuelve el número de vecinos
+	//Cuenta las células vivas adyacentes a la célula correspondiente a las coordenadas de i j en la matriz world_a
+	int cont = 0;
+	
+	for(int k = -1; k <= 1;k++){
+		for(int t = -1; t <= 1;t++){
+			if(!(k+i < 0 || k+i > W_SIZE_X-1 || t+j < 0 ||t+j > W_SIZE_Y-1||(k==0 && t==0))){
+				cont = cont + world_a[k+i][t+j];
+			}
+		}
+	}
+	
+			
+	return cont;
 }
 
-bool world_get_cell(/* Recibo un mundo y unas coordenadas */)
+bool world_get_cell(bool world_a[W_SIZE_X][W_SIZE_Y],int i,int j)
 {
-	/*
-	 * TODO: Devuelve el estado de la célula de posición indicada
+	/*	Devuelve el estado de la célula de posición indicada
 	 * (¡cuidado con los límites del array!)
 	 */
+	bool res = false;
+	int cont = 0;
+	
+	cont = world_count_neighbors(world_a,i,j);
+	
+	if(world_a[i][j]==false){
+		res = (cont == 3);
+	}else{
+		if(cont >= 2 && cont <= 3){
+			res = true;
+		}
+	}
+	return res;
 }
 
 void world_copy(bool world_a[W_SIZE_X][W_SIZE_Y],bool world_b[W_SIZE_X][W_SIZE_Y])
